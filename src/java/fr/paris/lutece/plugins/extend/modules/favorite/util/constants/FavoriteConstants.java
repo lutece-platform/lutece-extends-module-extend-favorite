802772cc75f89ad5bbb583b11ab78130d668bfc4
Rafik YAHIAOUI <rafik.yahiaoui@paris.fr>
2021-09-08 12:11:05 +0200

    format code

--- src/java/fr/paris/lutece/plugins/extend/modules/favorite/util/constants/FavoriteConstants.java
+++ src/java/fr/paris/lutece/plugins/extend/modules/favorite/util/constants/FavoriteConstants.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2002-2015, Mairie de Paris
+ * Copyright (c) 2002-2021, City of Paris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the favoriteing conditions
+ * modification, are permitted provided that the following conditions
  * are met:
  *
  *  1. Redistributions of source code must retain the above copyright notice
- *     and the favoriteing disclaimer.
+ *     and the following disclaimer.
  *
  *  2. Redistributions in binary form must reproduce the above copyright notice
- *     and the favoriteing disclaimer in the documentation and/or other materials
+ *     and the following disclaimer in the documentation and/or other materials
  *     provided with the distribution.
  *
  *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
